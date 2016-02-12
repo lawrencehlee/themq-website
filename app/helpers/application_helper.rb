fad1e3f9e498f941f336172bb745c200e8bf789b
Lawrence H Lee <Lee.LawrenceH@gmail.com>
2016-02-12 14:40:34 -0500

    Added Redcarpet gem and helper for it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def markdown(text)
+		md = Redcarpet::Markdown.new(renderer,
+			extensions = {
+				no_intra_emhpasis: false,
+				disable_indented_code_blocks: false,
+				fenced_code_blocks: true,
+			})
+		return md.render(text).html_safe
+	end
 end
