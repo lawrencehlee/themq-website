c9861ff2fde0ce6ac05bdba3e8be1d8540984a4d
Lawrence H Lee <Lee.LawrenceH@gmail.com>
2016-02-12 14:52:58 -0500

    Finish up markdown business with articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,9 @@
 module ApplicationHelper
 	def markdown(text)
-		md = Redcarpet::Markdown.new(renderer,
-			extensions = {
+		md = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
 				no_intra_emhpasis: false,
 				disable_indented_code_blocks: false,
-				fenced_code_blocks: true,
-			})
-		return md.render(text).html_safe
+				fenced_code_blocks: true)
+		md.render(text).html_safe
 	end
 end
