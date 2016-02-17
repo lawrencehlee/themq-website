e6a568ea2bf02172dd01974e7b5fa656a5eb9804
Barak Tzori <barak.tzori@gmail.com>
2016-02-17 17:18:33 -0500

    Add pcp logic and slight style

--- app/controllers/ed_pcps_controller.rb
+++ app/controllers/ed_pcps_controller.rb
@@ -5,6 +5,15 @@ class EdPcpsController < ApplicationController
 
   def show
     @ed_pcp = EdPcp.find(params[:id])
+   
+    @ed_pcps = Array.new
+    @ed_pcps << @ed_pcp
+
+
+    if @ed_pcp.point == "1"
+        @cp = EdPcp.find(@ed_pcp.crspnd_point_id)
+        @ed_pcps << @cp
+    end
   end
 
 end
--- app/models/ed_pcp.rb
+++ app/models/ed_pcp.rb
@@ -1,2 +1,7 @@
 class EdPcp < ActiveRecord::Base
+    def get_ed_pcp_text
+ 	        filepath = File.join(
+		        Rails.root, 'app', 'assets', 'articles', self.text)
+        File.read(filepath).encode("UTF-8", :invalid=>:replace)       
+    end
 end
