958787ea87bef3d3a48acc90cc8213e8a2bbbb68
Barak Tzori <barak.tzori@gmail.com>
2016-03-01 20:52:50 -0500

    Added text teaser, author name, & date to index

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,20 @@ class Article < ActiveRecord::Base
 			Rails.root, 'app', 'assets', 'articles', self.text)
     File.read(filepath).encode("UTF-8", :invalid=>:replace)
   end
+
+	def get_article_text_teaser
+		filepath = File.join(
+			Rails.root, 'app', 'assets', 'articles', self.text)
+		n = 1;
+		open(filepath) do |f|
+			lines = []
+			n.times do
+				line = f.gets || break
+				lines << line
+			end
+			firstLine = lines.first
+			firstLine.split(' ')[0, 25].join(" ") + "...";
+
+		end
+	end
 end
