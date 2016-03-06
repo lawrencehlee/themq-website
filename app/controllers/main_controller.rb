176044cf9ee719d8f2e33957e579db0e918b75b3
Lawrence H Lee <Lee.LawrenceH@gmail.com>
2016-03-06 00:28:49 -0500

    Add articles and graphics to slideshow

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,9 @@
 class MainController < ApplicationController
   def index
+		# TODO: In the future, we're going to hand-select the featured
+		# pieces in the slideshow
+		@featured_content = Array.new
+		@featured_content << Article.find(1)
+		@featured_content << Article.find(2)
   end
 end
