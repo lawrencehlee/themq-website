ed1e7e41145ff1b741df63390f68949ca7ecb2de
Lawrence H Lee <Lee.LawrenceH@gmail.com>
2016-03-10 21:04:25 -0500

    Change image paths to not be hardcoded in db

--- app/models/ed_pcp.rb
+++ app/models/ed_pcp.rb
@@ -1,7 +1,15 @@
 class EdPcp < ActiveRecord::Base
-    def get_ed_pcp_text
- 	        filepath = File.join(
-		        Rails.root, 'app', 'assets', 'articles', self.text)
-        File.read(filepath).encode("UTF-8", :invalid=>:replace)       
-    end
+	IMAGE_SUBDIRECTORY = 'ed_pcps'
+
+	def get_ed_pcp_text
+		filepath = File.join(
+			Rails.root, 'app', 'assets', 'articles', self.text)
+		File.read(filepath).encode("UTF-8", :invalid=>:replace)
+	end
+
+	def get_relative_author_image_path
+		issue = Issue.find(self.issue_id)
+		issue_string = "#{issue.volume_no}.#{issue.issue_no}"
+		"#{issue_string}/#{IMAGE_SUBDIRECTORY}/#{self.author_image}"
+	end
 end
