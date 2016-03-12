0ecf4d128486b830e4b8ecdd0c5ec6ecbf3688df
Lawrence H Lee <Lee.LawrenceH@gmail.com>
2016-03-11 20:32:46 -0500

    Flesh out structure for more shhhhhhhh

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,5 +5,8 @@ class MainController < ApplicationController
 		@featured_content = Array.new
 		@featured_content << Article.find(1)
 		@featured_content << Article.find(2)
+
+		@more_shit_1 = Article.find(1)
+		@more_shit_2 = Article.find(2)
   end
 end
