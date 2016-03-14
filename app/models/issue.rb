91165c96e7200f11ac283e771f91f27284163881
Lawrence H Lee <Lee.LawrenceH@gmail.com>
2016-03-14 17:18:28 -0400

    Spruce up slideshow

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,13 @@
 class Issue < ActiveRecord::Base
+	def get_full_issue_name
+		"Volume #{self.volume_no} Issue #{self.issue_no}"
+	end
+
+	def get_abbreviated_issue_name
+		"#{self.volume_no}.#{self.issue_no}"
+	end
+
+	def get_human_readable_date
+		date.strftime('%B %-d, %Y')
+	end
 end
