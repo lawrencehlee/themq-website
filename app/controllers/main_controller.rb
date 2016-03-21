8a7b08db42ebdd80af699b453a4e7c33be76eea1
Lawrence H Lee <Lee.LawrenceH@gmail.com>
2016-03-21 01:37:58 -0400

    Create static method in issue get_latest_issue and use it in main controller to get a random top ten

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 class MainController < ApplicationController
   def index
+		@current_issue = Issue.get_latest_issue
+
 		# TODO: In the future, we're going to hand-select the featured
 		# pieces in the slideshow
 		@featured_content = Array.new
@@ -8,5 +10,7 @@ class MainController < ApplicationController
 
 		@more_shit_1 = Article.find(1)
 		@more_shit_2 = Article.find(2)
+
+		@random_top_ten = TopTen.get_random_from_issue(@current_issue)
   end
 end
--- app/models/top_ten.rb
+++ app/models/top_ten.rb
@@ -1,12 +1,12 @@
 class TopTen < ActiveRecord::Base
 	# Static method that gets all top tens from the specified issue
-	def self.get_all_from_issue(issue_id)
-		TopTen.where(issue_id: issue_id)
+	def self.get_all_from_issue(issue)
+		TopTen.where(issue_id: issue.issue_id)
 	end
 
 	# Static method that gets a random top ten from the specified issue
-	def self.get_random_from_issue(issue_id)
-		all_from_issue = TopTen.get_all_from_issue(issue_id)
+	def self.get_random_from_issue(issue)
+		all_from_issue = TopTen.get_all_from_issue(issue)
 		all_from_issue.offset(rand(all_from_issue.count)).first
 	end
 
