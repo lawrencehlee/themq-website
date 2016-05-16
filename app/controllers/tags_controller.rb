a448df9ef450fee44de40b3f9153390f56bf4a5e
Barak Tzori <barak.tzori@gmail.com>
2016-05-16 13:20:04 -0400

    Add Friendly ids to tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,7 +5,7 @@ class TagsController < ApplicationController
 	end
 
 	def show
-		@tag = Tag.find(params[:id])
+		@tag = Tag.friendly.find(params[:id])
 		@articles = @tag.get_all_articles_with_tag
 		@ed_pcps = @tag.get_all_ed_pcps_with_tag
 		@features = @tag.get_all_features_with_tag
