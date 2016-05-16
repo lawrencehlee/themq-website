3adac138f05dd37d660976210aefc29eed09212d
Barak Tzori <barak.tzori@gmail.com>
2016-05-16 11:52:29 -0400

    Lightbox images for features not working yet

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -9,7 +9,8 @@ class FeaturesController < ApplicationController
 
 	def show
 		@feature = Feature.find(params[:id])
-		@feature_path = '"' + @feature.get_relative_feature_path + '"'
-		@width = FastImage.size(@feature_path)
+		@feature_path = request.protocol + request.host_with_port + "/assets/" + @feature.get_relative_feature_path
+		@width = 1024
+		@height = 768
 	end
 end
--- app/models/feature.rb
+++ app/models/feature.rb
@@ -6,4 +6,5 @@ class Feature < ActiveRecord::Base
 		issue_string = "#{issue.volume_no}.#{issue.issue_no}"
 		"#{issue_string}/#{FEATURE_SUBDIRECTORY}/#{self.image}"
 	end
+
 end
