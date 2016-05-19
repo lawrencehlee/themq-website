de91f94e9f88542ed0a9b9cf0e48fdc63aaea9db
Barak Tzori <barak.tzori@gmail.com>
2016-05-18 20:35:33 -0400

    continue work on tag views

--- app/admin/top_ten.rb
+++ app/admin/top_ten.rb
@@ -1,5 +1,18 @@
 ActiveAdmin.register TopTen do
 
+	# find record with slug (friendly id)
+	controller do
+		def find_resource
+			begin
+				scoped_collection.where(slug: params[:id]).first!
+			rescue ActiveRecord::RecordNotFound
+				scoped_collection.find(params[:id])
+			end
+		end
+	end
+
+
+
 	permit_params :top_ten_id, :issue_id, :title, :no_of_entries
 
 	index do
