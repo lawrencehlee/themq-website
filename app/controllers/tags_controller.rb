6ae91957be2ac99c530aabd60f47ba6c5085bc4d
Barak Tzori <barak.tzori@gmail.com>
2016-05-21 14:30:49 -0400

    Move tag index and make ed_pcp tag view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,15 @@
 class TagsController < ApplicationController
 
 	def index
-		@tags = Tag.all
+		tag = Tag.find(params[:tag_id])
+		if tag == nil
+			tag = Tag.offest(rand(Tag.count)).first
+		end
+		@articles = tag.get_all_articles_with_tag
+		@ed_pcps = tag.get_all_ed_pcps_with_tag
+		@features = tag.get_all_features_with_tag
+		@top_tens = tag.get_all_top_tens_with_tag
+		@features = tag.get_all_features_with_tag
 	end
 
 	def show
