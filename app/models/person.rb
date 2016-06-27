df17baed8f3cc95644711b980004197cb9216b59
Barak Tzori <barak.tzori@gmail.com>
2016-06-27 16:21:12 -0700

    update person model for sidebar

--- app/models/person.rb
+++ app/models/person.rb
@@ -27,4 +27,16 @@ class Person < ActiveRecord::Base
 		self.name.gsub(/\s+/m, ' ').strip.split(" ").first
 	end
 
+  def get_more_content(limit, content_types, filter, excluded)
+    more_content = Array.new
+    this_author_filter = "person_id = #{self.person_id}"
+    new_filter = [filter, this_author_filter].join(' AND ')
+
+    content_types.each do |content_type|
+      more_content += content_type.where(new_filter)
+    end
+
+    more_content.delete(excluded)
+    more_content.sample(limit)
+  end
 end
