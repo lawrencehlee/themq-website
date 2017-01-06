615fd34293d0b79a0ef14f49eb146ceb7e64c537
Lawrence Lee <Lee.LawrenceH@gmail.com>
2017-01-05 17:40:15 -0800

    Add functional tests (#103)
    * Remove fixtures and put in first functional articles tests
    
    * Add basic functional tests for most pages

--- test/controllers/top_tens_controller_test.rb
+++ test/controllers/top_tens_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class TopTensControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get show" do
+    get(:show, {'id' => 25})
+    assert_response :success
+  end
+
+  test "should get random" do
+    get :random
+    assert_response :success
+  end
 end
