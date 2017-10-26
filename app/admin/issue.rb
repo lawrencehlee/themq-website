2f5f28e8aea4074c363dbdbb1255ea6ee1f91544
Lawrence Lee <Lee.LawrenceH@gmail.com>
2017-10-25 17:38:17 -0700

    Overhaul issue admin

--- app/admin/issue.rb
+++ app/admin/issue.rb
@@ -1,8 +1,9 @@
 ActiveAdmin.register Issue do
+  remove_filter :current
 
   permit_params :issue_id, :volume_no, :issue_no, :sub_issue_no, :date, :staff_photo,
     :staff_photo_caption, :eic_note, :booster_club, :brief_title, :masthead, :celeb_quote, :celeb,
-    :celeb_photo, :current
+    :celeb_photo, :current, :issuu_link
 
   index do
     selectable_column
@@ -18,6 +19,7 @@ ActiveAdmin.register Issue do
     column :celeb
     column :celeb_photo
     column :current
+    column :issuu_link
     actions
   end
 
@@ -26,8 +28,8 @@ ActiveAdmin.register Issue do
       f.input :volume_no
       f.input :issue_no
       f.input :sub_issue_no
-      f.input :date, as: :string
-      f.input :staff_photo
+      f.input :date, as: :datepicker
+      f.input :staff_photo, as: :file
       f.input :staff_photo_caption
       f.input :eic_note
       f.input :booster_club
@@ -35,9 +37,68 @@ ActiveAdmin.register Issue do
       f.input :masthead
       f.input :celeb_quote
       f.input :celeb
-      f.input :celeb_photo
+      f.input :celeb_photo, as: :file
+      f.input :issuu_link
       f.input :current
     end
     f.actions
+  end
+
+  controller do
+    def create
+      attrs = permitted_params[:issue]
+      @issue = Issue.new(attrs)
+      staff_photo = attrs[:staff_photo]
+      celeb_photo = attrs[:celeb_photo]
+
+      unless staff_photo.nil?
+        @issue.staff_photo = staff_photo.original_filename
+      end
+      unless celeb_photo.nil?
+        @issue.celeb_photo = celeb_photo.original_filename
+      end
+
+      @issue.save!
+
+      # Don't upload images until issue saves successfully
+      unless staff_photo.nil?
+        # Takes the StringIO version of the image, which is why we call image.open
+        IssueStorageAdapter.upload_photo(@issue.volume_no, @issue.issue_no, @issue.staff_photo, staff_photo.open)
+      end
+      unless celeb_photo.nil?
+        # Takes the StringIO version of the image, which is why we call image.open
+        IssueStorageAdapter.upload_photo(@issue.volume_no, @issue.issue_no, @issue.celeb_photo, celeb_photo.open)
+      end
+
+      redirect_to resource_url
+    end
+
+    def update
+      attrs = permitted_params[:issue]
+      @issue = Issue.find(resource.issue_id)
+      staff_photo = attrs[:staff_photo]
+      celeb_photo = attrs[:celeb_photo]
+
+      unless staff_photo.nil?
+        attrs[:staff_photo] = staff_photo.original_filename
+      end
+      unless celeb_photo.nil?
+        attrs[:celeb_photo] = celeb_photo.original_filename
+      end
+
+      @issue.update!(attrs)
+
+      # Don't upload images until issue saves successfully
+      unless staff_photo.nil?
+        # Takes the StringIO version of the image, which is why we call image.open
+        IssueStorageAdapter.upload_photo(@issue.volume_no, @issue.issue_no, staff_photo.original_filename, staff_photo.open)
+      end
+      unless celeb_photo.nil?
+        # Takes the StringIO version of the image, which is why we call image.open
+        IssueStorageAdapter.upload_photo(@issue.volume_no, @issue.issue_no, celeb_photo.original_filename, celeb_photo.open)
+      end
+
+      redirect_to resource_url
+    end
   end
 end
